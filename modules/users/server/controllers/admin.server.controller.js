6b2e06cfc42da09825920d516772236af95e666b
kevdaci <kevdaci@gmail.com>
2017-10-30 14:34:20 -0400

    Finally edit the bugs for the admin to apply a new user to the system.

--- modules/users/server/controllers/admin.server.controller.js
+++ modules/users/server/controllers/admin.server.controller.js
@@ -45,13 +45,10 @@ exports.adminsignup = function (req, res) {
   // For security measurement we remove the roles from the req.body object
   delete req.body.roles;
 
-  // Init user and add missing fields
   var user = new User(req.body);
   user.provider = 'local';
-  user.displayName = user.firstName + ' ' + user.lastName;
   user.approvedStatus = true;
 
-  // Then save the user
   user.save(function (err) {
     if (err) {
       return res.status(422).send({
